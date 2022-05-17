d18700f2240f4b2724fbe4ee9f00eff7840c1505
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-17 10:31:44 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movieData, onMovieClick } = this.props;
 
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+    return 
+    
+    <div className="movie-title" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+    <div className="movie-image" onClick={() => { onMovieClick(movie); }}>{movie.ImgPath}</div>;
+    <div className="movie-summary" onClick={() => { onMovieClick(movie); }}>{movie.Description}</div>;
   }
 }
