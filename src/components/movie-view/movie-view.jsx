0cf9d981b2dca89f7d833d6dc11b4a4e72091767
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-17 14:59:26 -0500

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,57 +1,24 @@
-import React from 'react';
+import React from "react";
 
 export class MovieView extends React.Component {
-
   render() {
     const { movie, onBackClick } = this.props;
 
     return (
       <div className="movie-view">
-          <div className="movie-image">
-            <img src={movie.ImgPath} />
-          </div>
-          <div className="movie-title">
-            <span className="label">Title: </span>
-            <span className="value">{movie.Title}</span>
-          </div>
-          <div className="movie-summary">
-            <span className="label">Description: </span>
-            <span className="value">{movie.Description}</span>
-          </div>
-          <div className="movie-genre">
-            <span className="label">Genre: </span>
-            <span className="value">{movie.Genre.Name}</span>
-          </div>
-          <div className="movie-rating">
-            <span className="label">Movie Rating: </span>
-            <span className="value">{movie.Rating}</span>
-          </div>
-          <div className="movie-release">
-            <span className="label"> Theater Release Date: </span>
-            <span className="value">{movie.ReleaseDate}</span>
-          </div>
-          <div className="movie-director">
-            <span className="label">Director: </span>
-            <span className="value">{movie.Director.Name}</span>
-          </div>
-          <div className="movie-director__bio">
-            <span className="label">Director biography: </span>
-            <span className="value">{movie.Director.Biography}</span>
-          </div>
-          <div className="movie-director__DOB">
-            <span className="label">Director Birthdate: </span>
-            <span className="value">{movie.Director.Birthdate}</span>
-          </div>
-          <div className="movie-director__POB">    /* POC is place of birth */
-            <span className="label">Director Birthplace: </span>
-            <span className="value">{movie.Director.Birthplace}</span>
-          </div>
-          <div className="movie-director__DOD">    /* DOD is date of death */
-            <span className="label">Director Deathdate: </span>
-            <span className="value">{movie.Director.Deathdate}</span>
-          </div>
-          <button onClick={() => { onBackClick(null);}}>Back</button>
-      </div>    
+        <div className="movie-image">
+          <img src={movie.ImgPath} />
+        </div>
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-summary">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+      <button onClick={() => { onBackClick(null);}}> Back </button>
+      </div>
     );
   }
 }
