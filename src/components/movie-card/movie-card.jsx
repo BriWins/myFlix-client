0c7e5b857dac8bb1744c99908367963dd81b1751
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-18 13:22:04 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export class MovieCard extends React.Component {
   render() {
@@ -17,3 +18,10 @@ export class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
