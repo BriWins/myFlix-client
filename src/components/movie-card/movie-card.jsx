af7d5941cd241356333690dda2adda8a0163a197
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-19 14:48:47 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {Button, Card} from "react-bootstrap/";
+import axios from "axios";
 
 export class MovieCard extends React.Component {
   render() {
