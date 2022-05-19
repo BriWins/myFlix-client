b0290c93d6b70ca0a9931d3210c49c701a90ef22
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-19 14:26:27 -0500

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -19,57 +19,65 @@ const handleRegistration = (e) => {
   };
 
   return (
-    <Form>
-      <Form.Group>
-        <Form.Label>Username:</Form.Label>
-            <Form.Control 
-            type="text" 
-            value={username} 
-            onChange={e => setUsername(e.target.value)} 
-            placeholder="Input a username"
-            required />
-        </Form.Group>
 
-        <Form.Group>
-          <Form.Label>Password:</Form.Label>
-          <Form.Control 
-            type="password" 
-            value={password} 
-            onChange={e => setPassword(e.target.value)}
-            placeholder="Passwords must be a minimum of 8 characters"
-            required
-            minLength={8} />
-        </Form.Group>
+<Container>
+      <Row>
+        <Col>
+            <Form>
+              <Form.Group>
+                <Form.Label>Username:</Form.Label>
+                    <Form.Control 
+                    type="text" 
+                    value={username} 
+                    onChange={e => setUsername(e.target.value)} 
+                    placeholder="Input a username"
+                    required />
+                </Form.Group>
 
-        <Form.Group>
-          <Form.Label>Email:</Form.Label>
-          <Form.Control 
-            type="email" 
-            value={email} 
-            onChange={e => setEmail(e.target.value)}
-            placeholder="Input your email"
-            required />
-        </Form.Group>
-      
-        <Form.Group>
-          <Form.Label>Birthdate:</Form.Label>
-          <Form.Control 
-            type="birthdate" 
-            value={birthdate} 
-            onChange={e => setBirthdate(e.target.value)}
-            placeholder="Birthdate Format: XX/XX/XXXX" />
-        </Form.Group>
+                <Form.Group>
+                  <Form.Label>Password:</Form.Label>
+                  <Form.Control 
+                    type="password" 
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder="Passwords must be a minimum of 8 characters"
+                    required
+                    minLength={8} />
+                </Form.Group>
 
-        <Form.Group>
-          <Form.Label>Favorites:</Form.Label>
-          <Form.Control 
-            type="favorites" 
-            value={favorites} 
-            onChange={e => setFavorites(e.target.value)}
-            placeholder="Add favorite movies here!" />
-        </Form.Group>
-            <Button variant="danger" type="submit" onClick={handleRegistration}>Submit</Button>
-      </Form>
+                <Form.Group>
+                  <Form.Label>Email:</Form.Label>
+                  <Form.Control 
+                    type="email" 
+                    value={email} 
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder="Input your email"
+                    required />
+                </Form.Group>
+              
+                <Form.Group>
+                  <Form.Label>Birthdate:</Form.Label>
+                  <Form.Control 
+                    type="birthdate" 
+                    value={birthdate} 
+                    onChange={e => setBirthdate(e.target.value)}
+                    placeholder="Birthdate Format: XX/XX/XXXX" />
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Label>Favorites:</Form.Label>
+                  <Form.Control 
+                    type="favorites" 
+                    value={favorites} 
+                    onChange={e => setFavorites(e.target.value)}
+                    placeholder="Add favorite movies here!" />
+                </Form.Group>
+                <Button variant="danger" type="submit" onClick={handleRegistration}>Submit</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+    
   );
 }
 
