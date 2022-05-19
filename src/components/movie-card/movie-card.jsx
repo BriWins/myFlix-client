f1109928fa6a1a6e35c647ba02358d9c99eef0e4
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-19 10:25:13 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -11,7 +11,7 @@ export class MovieCard extends React.Component {
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src="movie.ImgPath" />
           <Card.Body>
-            <Card.Title>movie.Title</Card.Title>
+            <Card.Title>{movie.Title}</Card.Title>
             <Card.Text>{movie.Description}</Card.Text>
             <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
           </Card.Body>
