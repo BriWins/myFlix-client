fbbf3fe0384d6253c08c4b0656c84b3e6a73c460
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-20 13:01:15 -0500

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 
+import axios from "axios";
+
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
+    axios.post("https://peaceful-sierra-49110.herokuapp.com/login", {
+      Username: username,
+      Password: password
+    })
+    .then(response => {
+      const data = response.data;
+      props.onLoggedIn(data);
+    })
+    .catch(e => {
+      console.log("no such user")
+    });    
   };
 
   return (
