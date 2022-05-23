2a9d2b40427016057d35345b6b9cc5594d39de03
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-23 08:43:14 -0500

    Adding Login Form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+
 import axios from "axios";
 
 export function LoginView(props) {
@@ -21,17 +22,40 @@ export function LoginView(props) {
     });    
   };
 
+
+
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+<Form>
+  <Form.Group className="mb-3">
+    <Form.Label>Username</Form.Label>
+    <Form.Control type="text" placeholder="Enter username" />
+  </Form.Group>
+
+  <Form.Group className="mb-3">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Enter password" />
+  </Form.Group>
+  <Form.Group className="mb-3">
+    <Form.Check type="checkbox" label="Remember me" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+  Login
+  </Button>
+</Form>
+    
   );
+
+  // return (
+  //   <form>
+  //     <label>
+  //       Username:
+  //       <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+  //     </label>
+  //     <label>
+  //       Password:
+  //       <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+  //     </label>
+  //     <button type="submit" onClick={handleSubmit}>Submit</button>
+  //   </form>
+  // );
 }
