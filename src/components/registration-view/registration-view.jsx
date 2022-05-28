f56617556e8f0577b8b8970f89fa5d6d93ea7a2a
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-27 19:19:22 -0500

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import { Form, Button } from "react-bootstrap/";
 
@@ -22,8 +23,53 @@ export function LoginView(props) {
              isReq = false;
          }
          if(!password){
-             
+             setPasswordErr("Password Required");
+             isReq = false;
+         } else if (password.length < 8){
+             setPasswordErr("Password must be 8 characters long");
+             isReq = false;
          }
+         return isReq;
      }
 
-}
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const isReq = validate();
+    if(isReq) {
+      /* Send request to the server for authentication */
+      axios.post('YOUR_API_URL/login', {
+          Username: username,
+          Password: password
+      })
+      .then(response =>{
+          const data = response.data;
+          props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log('no such user')
+      });
+    }
+  };
+  
+    return (
+      <Form>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+          {/* code added here to display validation error */}
+          {usernameErr && <p>{usernameErr}</p>}
+  </Form.Group>
+  
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+          {/* code added here to display validation error */}
+          {passwordErr && <p>{passwordErr}</p>}
+  </Form.Group>
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+          Submit
+          </Button>
+      </Form>
+    )
+  }
