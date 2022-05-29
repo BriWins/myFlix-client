ec743e7f06c2aa8416d739a22920a73153ed7ef5
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-29 17:11:16 -0500

    Update navbar.jsx

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { Navbar, Container, Nav, Button } from "react-bootstrap/";
+
+const onLoggedOut = () => {
+    localStorage.clear();
+    window.open("/", "_self");
+}
+
+const isAuth = () => {
+    if(typeof window == "undefined") {
+        return false;
+    }
+    if (localStorage.getItem("token")) {
+        return localStorage.getItem("token");
+    } else {
+        return false;
+    }
+};
+
+return (
+    <Navbar className="main-nav" sticky="top" bg="dark" expand="lg" variant="dard">
+        <Container>
+            <Navbar.Brand className="navbar-logo" href="/">myFlixCinema</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-nabar-nav"/>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="m1-auto">
+                    { isAuth() && (
+                        <Nav.Link href={"/users/${user}"}>{user}</Nav.Link> )}
+                        { isAuth() && (
+                            <Button variant="link" onClick={() =>
+                            { this.onLoggedOut() }}>Logout</Button> )}
+                            { !isAuth() && (
+                                <Nav.Link href="/">Sign-in</Nav.Link>)}
+                                { !isAuth() && (
+                                    <Nav.Link href="/register">Sign-up</Nav.Link>
+                                )}
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+);
+                                
