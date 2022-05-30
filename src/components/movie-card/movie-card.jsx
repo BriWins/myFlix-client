89a4c6d8b15d6028ced11c81fa8285fe89f4f8e3
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-29 20:38:15 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -25,9 +25,9 @@ export class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    ImgPath: PropTypes.string.isRequired
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  Title: PropTypes.string.isRequired,
+  Description: PropTypes.string.isRequired,
+  ImgPath: PropTypes.string.isRequired
+  })
+  .isRequired, onMovieClick: PropTypes.func.isRequired
 };
