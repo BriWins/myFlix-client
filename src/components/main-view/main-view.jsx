978cc9bd18034b90877cbb433d2b6bc8380e0237
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-29 20:20:10 -0500

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import Menu from "../Menu/Menu";
 
 import { LoginView } from "../login-view/login-view";
 import { MovieView } from "../movie-view/movie-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { RegistrationView } from "../registration-view/registration-view";
+import { DirectorView } from "../director-view/director-view";
+import { GenreView } from "../genre-view/genre-view";
+
 
 import { Row, Col } from "react-bootstrap/"
 
 export class MainView extends React.Component {
+
   constructor() {
     super();
     this.state = {
@@ -18,6 +21,7 @@ export class MainView extends React.Component {
       selectedMovie: null,
     };
   }
+
   componentDidMount() {
     let accessToken = localStorage.getItem('token');
     if (accessToken !== null) {
@@ -28,9 +32,17 @@ export class MainView extends React.Component {
     }
   }
 
-  setSelectedMovie(newSelectedMovie) {
-    this.setState({
-      selectedMovie: newSelectedMovie,
+  getMovies(token) {
+    axios.get("https://stark-oasis-54313.herokuapp.com/movies", {
+      headers: {Authorization: 'Bearer ${token}'}
+    })
+    .then( response => {
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
     });
   }
 
@@ -46,75 +58,67 @@ export class MainView extends React.Component {
 
   }
 
-    getMovies(token) {
-      axios.get("https://stark-oasis-54313.herokuapp.com/movies", {
-        headers: {Authorization: 'Bearer ${token}'}
-      })
-      .then( response => {
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    }
-  
-
   render() {
     const { movies, user } = this.state;
-   
-
-    if (movies.length === 0) return <div className="main-view" />;
-
     return (
       <Router>
-        <Menu user={user} />
         <Container>
-        <Row className="main-view justify-content-md-center">
-          <Route path="/register" render={() => {
-          if (user) return <Redirect to="/" />
-           return <Col>
-          <RegistrationView />
-      </Col>
-}} />
-
-          <Route exact path="/" render ={ () => {
+          <Row className="main-view justify-content-md-center">
+            <Route exact path="/" render ={ () => {
              if (!user) return 
-            
-               <Col>
+                <Col>
                <LoginView onLoggedIn={user => this.onLoggedIn(user)}/>
                </Col>
                if ( movies.length === 0) return <div className="main-view" />;
-            
-            return movies.map(m => (
-              <Col md={3} key={m._id}>
-                <MovieCard movie={m} />
+                  return movies.map(m => (
+                    <Col md={3} key={m._id}>
+                      <MovieCard movie={m} />
+                    </Col>
+                  ))
+                  }} />
+            <Route path="/register" render={() => {
+              if (user) return <Redirect to="/" />
+              return 
+              <Col>
+                <RegistrationView />
               </Col>
-            ))
-          }} />
-          <Route path="/movies/:movieId" render={({ match }) => {
+                  }} />
+            <Route path="/movies/:movieId" render={({ match }) => {
+              if (user) return 
+            <Col>
+              <LoginView onLoggedIn={user => this.onLoggedIn(user)}/>
+            </Col>
+            if (movies.length === 0) return <div className="main-view" />;
             return <Col md={8}>
-              <MovieView movie={movies.find(m => m._id === match.params.movieID)} 
-              onBackClick={() => history.goBack()} />
+                <MovieView movie={movies.find(m => m._id === match.params.movieID)} 
+                onBackClick={() => history.goBack()} />
+              </Col>
+            }} />
+
+            <Route exact path="/directors/:names" render={({ match }) => {
+              if (user) return 
+            <Col>
+                <LoginView onLoggedIn={user => this.onLoggedIn(user)}/>
+            </Col>
+            if (movies.length === 0) return <div className="main-view" />;
+            return <Col md={8}>
+                <DirectorView director={movies.find(m => m.Director.Name === match.params.names).Director} 
+                onBackClick={() => history.goBack()} />
+            </Col>
+          }} /> 
+
+            <Route exact path="/genres/:genres" render={({ match, history }) => {
+              if (user) return 
+            <Col>
+                <LoginView onLoggedIn={user => this.onLoggedIn(user)}/>
+            </Col>
+            if (movies.length === 0) return <div className="main-view" />;
+            return <Col md={8}>
+              <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} 
+              onBackClick={() => history.goBack()}/>
             </Col>
           }} />
-
-      <Route exact path="/directors/:names" render={({ match }) => {
-        if ( movies.length === 0) return <div className="main-view" />;
-      return <Col md={8}>
-        <DirectorView director={movies.find(m => m.Director.Name === match.params.names).Director} onBackClick={() => history.goBack()}/>
-      </Col>
-    }
-    } /> 
-
-      <Route exact path="/genres/:genres" render={({ match, history }) => {
-        if ( movies.length === 0) return <div className="main-view" />;
-      return <Col md={8}>
-        <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()}/>
-      </Col>
-    }
-  } />
+          
       <Route exact path={"/users/${user}"} render={({ match, history }) => {
         if (!user) return <Redirect to="/"/>
       return <Col>
