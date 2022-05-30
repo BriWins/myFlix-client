d84502439371212803befe0997c77662374e5b3c
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-29 20:35:14 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,28 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 import { Button, Card } from "react-bootstrap/";
 
-import { Link } from "react-router-dom";
-
 export class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
-
+      const { movie } = this.props;
     return (
-      <Card>
-         <Card.Img crossorigin="anonymous" src={movie.ImgPath} className="movie-image" onClick={() => { onMovieClick(movie);}}/>
-           <Card.Body>
-             <Card.Title> {movie.Title} </Card.Title>
-             <Card.Text> {movie.Description}</Card.Text>
-          <Link to={"/movies/${movie._id"}>
-            <Button variant="link">Open</Button>
-          </Link>
-          </Card.Body>
+          <Card>
+              <Card.Img crossorigin="anonymous" src={movie.ImgPath} className="movie-image" 
+              onClick={() => { onMovieClick(movie);}}/>
+              <Card.Body>
+                <Card.Title> {movie.Title} </Card.Title>
+                <Card.Text> {movie.Description}</Card.Text>
+                  <Link to={"/movies/${movie._id"}>
+                    <Button variant="link">Open</Button>
+                  </Link>
+              </Card.Body>
           </Card>
-    );
+      );
+    }
   }
-}
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
