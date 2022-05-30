a2a447b057a69b43deaf979dc7edfea51fc8e922
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-29 20:56:14 -0500

    Update profile-view.jsx

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./profile-view.scss";
+
+import { Button, Form, Card } from "react-bootstrap/";
