4f08f8e87d7b340caa302d6199299db0e5e9926a
BriWins <95152254+BriWins@users.noreply.github.com>
2022-05-31 10:17:16 -0500

    Update director-view.jsx

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Button, Col, Container, Row, Col } from "react-bootstrap/";
+
+import './director-view.scss';
+
+export class DirectorView extends React.Component {
+  render() {
+    const { director, onBackClick } = this.props;
+ 
+    return (
+      <Container className="director-view">
+        <Row>
+          <Col className="label">Director: </Col>
+          <Col className="value">{director.Name}</Col>
+        </Row>
+        <Row className="mt-3">
+          <Col className="label">Bio: </Col>
+          <Col className="value">{director.Biography}</Col>
+        </Row>
+        <Row className="mt-3">
+          <Col className="label">Date of Birth: </Col>
+          <Col className="value">{director.Birthdate}</Col>
+        </Row>
+        <Row className="mt-3">
+          <Col className="label">Birthplace: </Col>
+          <Col className="value">{director.Birthplace}</Col>
+        </Row>
+        <Row className="mt-3">
+          <Col className="label">Deathplace: </Col>
+          <Col className="value">{director.Deathplace}</Col>
+        </Row>
+        <Button className="d-block mt-3" onClick={() => { onBackClick(null); }} variant="warning">Back</Button>
+      </Container>
+    )
+  }
+}
+
+DirectorView.propTypes = {
+  director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Biography: PropTypes.string.isRequired,
+    Birthdate: PropTypes.string.isRequired,
+    Birthplace: PropTypes.string.isRequired,
+    Deathplace: PropTypes.string
+  }).isRequired
+};
