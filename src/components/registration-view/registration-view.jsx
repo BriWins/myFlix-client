debc2b080155946c4b22f4d5096ef7c544c2446e
BriWins <95152254+BriWins@users.noreply.github.com>
2022-06-02 10:42:56 -0500

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,126 +1,146 @@
-import React, { useState } from "react";
+mport React, { useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { Form, Button, Row, Col, Container } from "react-bootstrap/";
 
-import "./registration-view.scss"
+import "./registration-view.scss";
 
 export function RegistrationView(props) {
-    const [ username, setUsername ] = useState("");
-    const [ password, setPassword ] = useState("");
-    const [ email, setEmail ] = useState("");
-    const [ birthdate, setBirthdate ] = useState("");
-    
-    /* a hook is declared for each input*/
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthdate, setBirthdate] = useState("");
 
-    const [ usernameErr, setUsernameErr ] = useState("");
-    const [ passwordErr, setPasswordErr ] = useState("");
-    const [ emailErr, setEmailErr ] = useState("");
-    const [ birthdateErr, setBirthdateErr ] = useState("");
-    
+  /* a hook is declared for each input*/
 
-     /* validating user inputs*/
+  const [usernameErr, setUsernameErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+  const [emailErr, setEmailErr] = useState("");
+  const [birthdateErr, setBirthdateErr] = useState("");
 
-     const validate = () => {
-         let isReq = true;
-            if(!username){
-                setUsernameErr("Username Required");
-                isReq = false;
-            } else if (username.length < 6){
-                setUsernameErr("Username must be 6 characters long");
-                isReq = false;
-            }
-            if(!password){
-                setPasswordErr("Password Required");
-                isReq = false;
-            } else if (password.length < 8){
-                setPasswordErr("Password must be 8 characters long");
-                isReq = false;
-            }
-            if(!email){
-                setEmailErr("Email Required");
-                isReq = false;
-            } else if (email.indexOf("@") === -1){
-                setEmailErr("Please enter a valid email address");
-                isReq = false;
-            }
+  /* validating user inputs*/
 
-         return isReq;
-     }
-
-
-const handleSubmit = (e) => {
-    e.preventDefault();
-        const isReq = validate();
-        if(isReq) {
-        /* Send request to the server for authentication */
-        axios.post("https://glacial-shore-06302.herokuapp.com/users/register", {
-            Username: username,
-            Password: password,
-            Email: email,
-            Birthdate: birthdate
-        })
-        .then(response =>{
-            const data = response.data;
-            console.log(data);
-            alert("Registration successful!");
-            window.open("/","_self");
-        })
-        .catch(response => {
-            console.error(response);
-            alert("unable to register");
-        })
+  const validate = () => {
+    let isReq = true;
+    if (!username) {
+      setUsernameErr("Username Required");
+      isReq = false;
+    } else if (username.length < 6) {
+      setUsernameErr("Username must be 6 characters long");
+      isReq = false;
     }
-};
-  
-    return (
-        <Row className="mt-5">
-            <Col md={12}>
-                <Form>
-                    <h3>Sign Up</h3>
-                    <p></p>
-                <Form.Group controlId="formUsername">
-                    <Form.Label>Username:</Form.Label>
-                    <Form.Control type="text" placeholder="Enter username" value={username} 
-                    onChange={e => setUsername(e.target.value)} />
-                    {values.usernameErr && <p>{values.usernameErr}</p>}
-                </Form.Group>
-  
-                <Form.Group controlId="formPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter password" value={password} 
-                    onChange={e => setPassword(e.target.value)} />
-                    {values.passwordErr && <p>{values.passwordErr}</p>}
-                </Form.Group>
+    if (!password) {
+      setPasswordErr("Password Required");
+      isReq = false;
+    } else if (password.length < 8) {
+      setPasswordErr("Password must be 8 characters long");
+      isReq = false;
+    }
+    if (!email) {
+      setEmailErr("Email Required");
+      isReq = false;
+    } else if (email.indexOf("@") === -1) {
+      setEmailErr("Please enter a valid email address");
+      isReq = false;
+    }
 
-                <Form.Group controlId="formEmail">
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email address" value={email} 
-                    onChange={e => setEmail(e.target.value)} />
-                    {values.emailErr && <p>{values.emailErr}</p>}
-                </Form.Group>
-
-                <Form.Group controlId="formBirthdate">
-                    <Form.Label>Date of Birth</Form.Label>
-                    <Form.Control type="birthdate" placeholder="Date of Birth is optional" value={birthdate} 
-                    onChange={e => setBirthdate(e.target.value)} />
-                    {values.birthdateErr && <p>{values.birthdateErr}</p>}
-                </Form.Group>
-
-            <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
-                <p></p>
-                <p>Already Registered? <Link to={"/"}> Sign In</Link> here!</p>
-                </Form>
-            </Col>
-        </Row>
-)}
-
-  RegistrationView.PropTypes = {
-        register: PropTypes.shape({
-        Username: PropTypes.string.isRequired,
-        Password: PropTypes.string.isRequired,
-        Email: PropTypes.string.isRequired,
-        Birthdate: PropTypes.string
-    })
+    return isReq;
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isReq = validate();
+    if (isReq) {
+      /* Send request to the server for authentication */
+      axios
+        .post("https://glacial-shore-06302.herokuapp.com/users/register", {
+          Username: username,
+          Password: password,
+          Email: email,
+          Birthdate: birthdate,
+        })
+        .then((response) => {
+          const data = response.data;
+          console.log(data);
+          alert("Registration successful!");
+          window.open("/", "_self");
+        })
+        .catch((response) => {
+          console.error(response);
+          alert("unable to register");
+        });
+    }
+  };
+
+  return (
+    <Row className="mt-5">
+      <Col md={12}>
+        <Form>
+          <h3>Sign Up</h3>
+          <p></p>
+          <Form.Group controlId="formUsername">
+            <Form.Label>Username:</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            {usernameErr && <p>{usernameErr}</p>}
+          </Form.Group>
+
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {passwordErr && <p>{passwordErr}</p>}
+          </Form.Group>
+
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {emailErr && <p>{emailErr}</p>}
+          </Form.Group>
+
+          <Form.Group controlId="formBirthdate">
+            <Form.Label>Date of Birth</Form.Label>
+            <Form.Control
+              type="birthdate"
+              placeholder="Date of Birth is optional"
+              value={birthdate}
+              onChange={(e) => setBirthdate(e.target.value)}
+            />
+            {birthdateErr && <p>{birthdateErr}</p>}
+          </Form.Group>
+
+          <Button variant="primary" type="submit" onClick={handleSubmit}>
+            Submit
+          </Button>
+          <p></p>
+          <p>
+            Already Registered? <Link to={"/"}> Sign In</Link> here!
+          </p>
+        </Form>
+      </Col>
+    </Row>
+  );
+}
+
+RegistrationView.PropTypes = {
+  register: PropTypes.shape({
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+    Email: PropTypes.string.isRequired,
+    Birthdate: PropTypes.string,
+  }),
+};
