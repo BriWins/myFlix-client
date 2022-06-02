932f5eadff5fa5c1dac587504fb9dd60a8005ede
BriWins <95152254+BriWins@users.noreply.github.com>
2022-06-02 10:43:28 -0500

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,4 @@
-mport React, { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
